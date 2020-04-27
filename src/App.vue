c283ba6ad3055cefae2a97476e3834a07575c5a5
Flavio Santana <f.o.santana@gmail.com>
2020-04-27 11:51:14 -0300

    Manipulando inputs do tipo checkbox

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,29 @@
 
             <div class="form-group">
               <label>Nome:</label>
-              <input type="text" class="form-control" placeholder="Seu nome">
+              <input 
+                type="text" 
+                class="form-control" 
+                placeholder="Seu nome"
+                v-model.trim="formulario.nome">
             </div>
 
             <div class="form-group">
               <label>Endereço de email:</label>
-              <input type="email" class="form-control" placeholder="Seu email">
+              <input 
+                type="email" 
+                class="form-control" 
+                placeholder="Seu email"
+                v-model.lazy.trim="formulario.email">
             </div>
 
             <div class="form-group">
               <label>Idade:</label>
-              <input type="number" class="form-control" placeholder="Sua idade">
+              <input 
+                type="number" 
+                class="form-control" 
+                placeholder="Sua idade"
+                v-model.number="formulario.idade">
             </div>
 
             <div class="form-group">
@@ -39,12 +51,24 @@
               <p>Gênero:</p>
 
               <div class="form-check form-check-inline">
-                <input type="radio" class="form-check-input" value="Masculino">
+                <input 
+                  type="radio" 
+                  id="mas" 
+                  name="genero"
+                  class="form-check-input" 
+                  value="Masculino" 
+                  v-model="formulario.genero">
                 <label class="form-check-label">Masculino</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="radio" class="form-check-input" value="Feminino">
+                <input 
+                  type="radio" 
+                  id="fem"
+                  name="genero" 
+                  class="form-check-input" 
+                  value="Feminino" 
+                  v-model="formulario.genero">
                 <label class="form-check-label">Feminino</label>
               </div>
 
@@ -52,8 +76,12 @@
 
             <div class="form-group">
               <label>Ocupação:</label>
-              <select class="form-control" placeholder="Seu email">
-                <option>Selecione uma opção...</option>
+              <select class="form-control" placeholder="Seu email" v-model="formulario.ocupacao">
+                <option disabled>Selecione uma opção...</option>
+                <option>Pensionista</option>
+                <option>Funcionário Privado</option>
+                <option>Dor Lar</option>
+                <option>Profissional Liberal</option>
               </select>
             </div>  
 
@@ -62,22 +90,38 @@
               <p>Tecnologias:</p>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="JavaScript">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  value="JavaScript" 
+                  v-model="formulario.tecnologias">
                 <label class="form-check-label">JavaScript</label>
               </div>
 
-              <div class="form-check form-check-inline" value="Vue JS">
-                <input type="checkbox" class="form-check-input">
+              <div class="form-check form-check-inline">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  value="Vue JS" 
+                  v-model="formulario.tecnologias">
                 <label class="form-check-label">Vue JS</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="Vuex">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  value="Vuex" 
+                  v-model="formulario.tecnologias">
                 <label class="form-check-label">Vuex</label>
               </div>
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input" value="Vue Router">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  value="Vue Router" 
+                  v-model="formulario.tecnologias">
                 <label class="form-check-label">Vue Router</label>
               </div>
 
@@ -85,13 +129,19 @@
 
             <div class="form-group">
               <label>Resumo de perfil:</label>
-              <textarea class="form-control" placeholder="Conte-nos um pouco sobre você..."></textarea>
+              <textarea 
+                v-model.lazy.trim="formulario.resumoPerfil"
+                class="form-control" 
+                placeholder="Conte-nos um pouco sobre você..."></textarea>
             </div>
 
             <div class="form-group">
 
               <div class="form-check form-check-inline">
-                <input type="checkbox" class="form-check-input">
+                <input 
+                  type="checkbox" 
+                  class="form-check-input" 
+                  v-model="formulario.receberNotificacao">
                 <label class="form-check-label">Receber notificações por email</label>
               </div>
 
@@ -114,20 +164,29 @@
             <div class="card-header">Dados</div>
 
             <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Nome:</strong></li>
-              <li class="list-group-item"><strong>Email:</strong> </li>
-              <li class="list-group-item"><strong>Idade:</strong></li>
-              <li class="list-group-item"><strong>Gênero:</strong> </li>
-              <li class="list-group-item"><strong>Ocupação:</strong> </li>
-              <li class="list-group-item"><strong>Tecnologias:</strong> </li>
-              <li class="list-group-item"><strong>Biografia:</strong> </li>
-              <li class="list-group-item"><strong>Receber notificações?</strong> </li>
+              <li class="list-group-item"><strong>Nome:</strong>{{ formulario.nome }}</li>
+              <li class="list-group-item"><strong>Email:</strong> {{ formulario.email }}</li>
+              <li class="list-group-item"><strong>Idade:</strong>{{ formulario.idade }}</li>
+              <li class="list-group-item"><strong>Gênero:</strong> {{ formulario.genero }}</li>
+              <li class="list-group-item"><strong>Ocupação:</strong> {{ formulario.ocupacao }} </li>
+              <li class="list-group-item"><strong>Tecnologias:</strong> 
+                {{ formulario.tecnologias}}
+              </li>
+              <li class="list-group-item"><strong>Biografia:</strong> 
+                <pre>{{ formulario.resumoPerfil }}</pre>
+              </li>
+              
+              <li class="list-group-item">
+                <strong>Receber notificações?</strong> 
+                {{ formulario.receberNotificacao ? 'Sim' : 'Não'}} 
+              </li>
+
             </ul>
 
             <div class="card-header">Model</div>
 
             <div class="card-body">
-              <pre><code>{{ {'nome': 'Plínio Naves'} }}</code></pre>
+              <pre><code> {{ {'nome': formulario } }} </code></pre>
             </div>
 
           </div>
@@ -141,6 +200,24 @@
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      formulario :{
+        nome : '',
+        email : '',
+        idade : '',
+        genero :'',
+        ocupacao: '',
+        tecnologias : [],
+        resumoPerfil : '',
+        receberNotificacao : false,
+      }
+    }
+  }
+}
+</script>
 
 <style scoped>
   .btn {
